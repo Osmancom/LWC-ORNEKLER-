1b94c05f9c7f9e559d0a0262215b83b703cdf979
Osman Sener <comalios6134@gmail.com>
2023-05-01 17:07:03 +0300

    V5

---
+++ force-app/main/default/lwc/contactList2/contactList.js
@@ -0,0 +1,20 @@
+import { LightningElement, wire } from 'lwc';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController2.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
+const COLUMNS = [
+    { label: ' First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' }
+];
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
+}
